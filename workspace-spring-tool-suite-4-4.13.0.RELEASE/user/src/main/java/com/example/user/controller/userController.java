08ca0f50753e491a18ebf9fe95dc10178e17a5a6
Ankush Saini <ankushsaini@loginextsolutions.com>
2022-01-12 19:03:37 +0530

    backend

--- workspace-spring-tool-suite-4-4.13.0.RELEASE/user/src/main/java/com/example/user/controller/userController.java
+++ workspace-spring-tool-suite-4-4.13.0.RELEASE/user/src/main/java/com/example/user/controller/userController.java
@@ -1,9 +1,15 @@
 package com.example.user.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.user.modal.userModal;
@@ -89,6 +96,31 @@ public class userController {
 			user_repository.save(newUser);
 			
 			return new ResponseEntity<>(newUser,HttpStatus.OK);
+	}
+	
+	/*PAGINATION*/
+	@GetMapping("/user/pagination")
+	public ResponseEntity<Map<String,Object>> userPagination(@RequestParam(defaultValue="0") int page,@RequestParam(defaultValue="2") int size){
+		try {
+			List<userModal> users=new ArrayList<userModal>();
+			Pageable paging=PageRequest.of(page, size);
+			
+			Page<userModal> userPage=user_repository.findAll(paging);
+			
+			users=userPage.getContent();
+			Map<String, Object> response = new HashMap<>();
+			
+			response.put("users", users);
+			response.put("currentPage",userPage.getNumber());
+			response.put("totalUsers",userPage.getTotalElements());
+			response.put("totalPages",userPage.getTotalPages());
+			
+			return new ResponseEntity<>(response,HttpStatus.OK);
+			
+		}
+		catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
 
 }
